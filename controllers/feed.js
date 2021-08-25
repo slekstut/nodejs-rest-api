4e747ceac5bd654245d90ceb8dad1d9c09b8b37d
Sarunas <sarunas.lekstutis@gmail.com>
2021-08-25 14:02:57 +0300

    first commit

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,21 +1,54 @@
+const {
+ validationResult
+} = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
  res.status(200).json({
   posts: [{
+   _id: '1',
    title: 'First post',
-   content: "This is my first post."
+   content: "This is my first post.",
+   imageUrl: 'images/duck.jpg',
+   creator: {
+    name: 'Sarunas'
+   },
+   createdAt: new Date()
   }]
  });
 };
 
 exports.createPost = (req, res, next) => {
+ const errors = validationResult(req);
+ if (!errors.isEmpty()) {
+  const error = new Error('Validation failed, entered data is incorrect.');
+  error.statusCode = 422;
+  throw error;
+ }
  const title = req.body.title;
  const content = req.body.content;
- res.status(201).json({
-  message: "Post created successfully.",
-  post: {
-   id: new Date().toISOString(),
-   title: title,
-   content: content
-  }
+ const post = new Post({
+  title: title,
+  content: content,
+  imageUrl: 'images/duck.jpg',
+  creator: {
+   name: "Sarunas"
+  },
  });
+ post
+  .save()
+  .then(result => {
+   res.status(201).json({
+    message: "Post created successfully.",
+    post: result
+   });
+  })
+  .catch(err => {
+   if (!err.statusCode) {
+    err.statusCode = 500;
+   }
+   next(err);
+  })
+
 };
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
  res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,4 +20,16 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+ console.log(error);
+ const status = error.statusCode || 500;
+ const message = error.message;
+ res.status(status).json({ message: message });
+});
+
+// mongoose.connect('process.env.CONNECT_STRING')
+mongoose.connect('mongodb+srv://slekstut:password000%21@cluster0.kzkvm.mongodb.net/messages?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true } )
+.then(result => {
+ app.listen(8080);
+})
+.catch(err => console.log(err));
