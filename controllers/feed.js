a988c5eeef9d183ac6d66f74c28d3f0ab96a58f8
Sarunas <sarunas.lekstutis@gmail.com>
2021-08-29 15:12:56 +0300

    pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,11 +6,25 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then(posts => {
       res
         .status(200)
-        .json({ message: 'Fetched posts successfully.', posts: posts });
+        .json({
+          message: 'Fetched posts successfully.',
+          posts: posts,
+          totalItems: totalItems
+        });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -32,7 +46,7 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  const imageUrl = req.file.path.replace("\\" ,"/");;
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
@@ -88,7 +102,7 @@ exports.updatePost = (req, res, next) => {
   const content = req.body.content;
   let imageUrl = req.body.image;
   if (req.file) {
-    imageUrl = req.file.path.replace("\\","/");
+    imageUrl = req.file.path;
   }
   if (!imageUrl) {
     const error = new Error('No file picked.');
@@ -131,20 +145,19 @@ exports.deletePost = (req, res, next) => {
         throw error;
       }
       // Check logged in user
-      if (post.imageUrl) {
-        clearImage(post.imageUrl);
-      }
+      clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     })
     .then(result => {
       console.log(result);
-      res.status(200).json({ message: 'Deleted post.'})
+      res.status(200).json({ message: 'Deleted post.' });
     })
     .catch(err => {
-      if(!err.statusCode) {
+      if (!err.statusCode) {
         err.statusCode = 500;
       }
-    })
+      next(err);
+    });
 };
 
 const clearImage = filePath => {
