9a5a715a9182f677425c3240452c05c7d4809664
Sarunas <sarunas.lekstutis@gmail.com>
2021-09-06 17:15:39 +0300

    graphql load post

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { graphqlHTTP  } = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -47,6 +48,8 @@ app.use((req, res, next) => {
  }
  next();
 });
+
+app.use(auth);
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
